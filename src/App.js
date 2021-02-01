eb994084282878b12afff4693141572cd2a008f9
Absent77735 <sotges@ukr.net>
2021-02-01 15:52:35 +0200

    shop

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './index.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronRight, faChevronLeft, faCircle, faCheckCircle, faPlus, faCalculator } from '@fortawesome/free-solid-svg-icons';
+
+const PAGE_PRODUCTS = 'products';
+const PAGE_CART = 'cart';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [cart, setCart] = useState([]);
+  const [page, setPage] = useState(PAGE_PRODUCTS);
+  const [products] = useState([
+    {
+      name: 'Яблоко',
+      cost: '$10'
+    },
+    {
+      name: 'Банан',
+      cost: '$8'
+    },
+    {
+      name: 'Папайя',
+      cost: '$10'
+    }
+  ]);
+  const addToCart = (product) => {
+    setCart([...cart, {...product}]);
+  }
+  const removeFromCart = (productToRemove) =>{
+    setCart(cart.filter(product => product !== productToRemove))
+  }
+  const navigateTo = (nextPage) =>{
+    setPage(nextPage);
+  }
 
+const renderProducts = () => (<>
+  <h1>Продукты</h1>
+  <div>
+    {products.map((product) => (
+      <div>
+        <h3>{product.name}</h3>
+        <h4>{product.cost}</h4>
+        <button onClick={() => addToCart(product)}>Корзина</button>
+      </div>
+    ))}
+  </div>
+  </>)
+const renderCart = () => (<>
+  <h1>Корзина</h1>
+  <div>
+    {cart.map((product) => (
+      <div>
+        <h3>{product.name}</h3>
+        <h4>{product.cost}</h4>
+        <button onClick={() => removeFromCart(product)}>Удалять</button>
+    
+      </div>
+    ))}
+  </div>
+  </>)
+  return (
+    <div>
+      <header>
+        <button onClick={()=> navigateTo(PAGE_CART)}>Корзина ({cart.length})</button>
+        <button onClick={()=> navigateTo(PAGE_PRODUCTS)}>Посмотреть продукты</button>
+      </header>
+      {page === PAGE_PRODUCTS && renderProducts()}
+      {page === PAGE_CART && renderCart()}
+  <span>Всего: 0</span>
+      </div>
+      );
+}
 export default App;
